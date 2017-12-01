5873e1c31dce507a73b2b40957e5d74ec12ec151
pat viafore <patviafore@gmail.com>
2017-12-01 14:50:21 -0600

    Added a better explanation

--- 01/main.cpp
+++ 01/main.cpp
@@ -6,6 +6,10 @@
 #include "algo.h"
 #include "input.h"
 
+//a raw loop probably is way more readable in this case, since you loop indexes and (compare index+distance) % size
+//however, I wanted to see what it would be like without raw loops in the code
+//So, I'm instead rotating a copy by the distance, zipping the strings into a pair, and accumulating any pairs that match
+
 auto getSum(const std::string &input, size_t distance=1) {
     auto next = input;
     std::rotate(next.rbegin(), next.rbegin() + distance,  next.rend());
