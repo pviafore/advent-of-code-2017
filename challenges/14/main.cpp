95ef4828b9e85398add8bcf2896f8f6c631a2eb1
pat viafore <patviafore@gmail.com>
2017-12-18 19:16:56 -0600

    Refactoring cyrpto

--- challenges/14/main.cpp
+++ challenges/14/main.cpp
@@ -7,11 +7,14 @@
 
 auto getNumberOfSquaresLit(const std::string& input) {
     auto range = algo::getNumericRange(0,128);
-    auto strings = algo::map(range.begin(), range.end(), [input](int i) { return input + "-" + std::to_string(i);});
-    auto knotHashes = algo::map(strings.begin(), strings.end(), )
+    auto strings = algo::map(range, [input](int i) { return input + "-" + std::to_string(i);});
+    auto knotHashes = algo::map(strings, crypto::calculateDenseHash );
+    return knotHashes;
 }
 
 int main() {
     auto input = std::string("hfdlxzhv");
+    auto value = getNumberOfSquaresLit("input");
+    std::cout << value << "\n";
     return 0;
 }
